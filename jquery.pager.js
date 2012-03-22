f34f44afdf0d757705f7cdca34f802cff41bd716
Mark A. Stratman <stratman@gmail.com>
2012-03-22 14:10:39 -0500

    Fix allows you to call pager({...}) multiple times
    and have the pager rebuild itself from scratch

--- jquery.pager.js
+++ jquery.pager.js
@@ -313,13 +313,9 @@ function JqueryPager(target, options) {
         var all = this.each(function() {
             var obj = $(this).data('pager');
             if (typeof method == 'object' || ! method || ! obj) {
-                // TODO: why was it like this, rather than just
-                //       a simple if(!obj) {...}
                 var options = $.extend({}, $.fn.pager.defaults, method || {});
-                if (! obj) {
-                    obj = new JqueryPager($(this), options);
-                    $(this).data('pager', obj);
-                }
+                obj = new JqueryPager($(this), options);
+                $(this).data('pager', obj);
             } else {
                 if (typeof JqueryPager.prototype[method] == "function") {
                     rv = JqueryPager.prototype[method].apply(obj, Array.prototype.slice.call(args, 1));
